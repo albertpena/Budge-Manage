ae165025501bf929b6f64d76ca872456705e2f72
Alejandro Leo Peguero Mejia <lionjet19@gmail.com>
2012-11-14 19:52:25 -0400

    Clases Account y Loan
    Estas son las clases pertenecientes al paquete products

---
+++ src/source/products/Account.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package source.products;
+
+/**
+ *
+ * @author Alexateam
+ */
+public class Account {
+    
+    private double balance = 25000;
+    private String  accountNumber;
+    private String accountBank;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountBank() {
+        return accountBank;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAccountBank(String accountBank) {
+        this.accountBank = accountBank;
+    }
+    
+    
+    public  void add(){}
+    
+    
+    public Account get(String account)
+    {
+        return null;
+
+    }
+    
+    
+    public Account get()
+    {
+        return null;
+    }
+            
+    
+ 
+    
+}
---
+++ src/source/products/Loan.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package source.products;
+
+/**
+ *
+ * @author Alexateam
+ */
+public class Loan {
+  
+     
+    private double amount;
+    private int shares;
+    private double interest;
+    
+    public void create(){
+    
+        
+    }
+    
+    
+    public Loan get()
+    {
+        return null;
+    }
+
+    public double getAmount() {
+        
+        return amount;
+    }
+
+    public int getShares() {
+        return shares;
+    }
+
+    public double getInterest() {
+        return interest;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setShares(int shares) {
+        this.shares = shares;
+    }
+
+    public void setInterest(double interest) {
+            this.interest = interest;
+        
+    
+    
+}
+    
+    
+}
