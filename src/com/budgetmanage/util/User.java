8de808a045b98d56d8b328eef2ea579099076d8c
Fulvio <Fulvio@Fulvio-THINK>
2012-11-15 14:18:38 -0400

    Moving the class inside the package correct

--- src/com/budgetmanage/util/User.java
+++ src/com/budgetmanage/util/User.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.budgetmanage.user;
+package com.budgetmanage.util;
 import com.budgetmanage.util.Entities;
 
 /**
