45c3b34022a2a5d29e2c6c7d452f1bf0d5714706
Fulvio <Fulvio@Fulvio-THINK>
2012-11-15 12:37:45 -0400

    Create a new class on a new package called user inside com.budgetmanage.

---
+++ src/com/budgetmanage/user/User.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.budgetmanage.user;
+import com.budgetmanage.util.Entities;
+
+/**
+ * The User class have all about the User's management 
+ * @author Fulvio
+ */
+public class User implements Entities{
+    private String nombre;
+    private String lastName;
+    private String password;
+    private String userName;
+
+   
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+     @Override
+    public void add() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void modify(String name) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void delete(String name) {
+        /*This method without your name is delete its fuction is disable users
+         on the system*/
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
