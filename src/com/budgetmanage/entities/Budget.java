d71c558c8d5e2a4df517fb1dc97874a9feab921e
Nestor_Velasquez <Nestor_Velasquez@opmobile02>
2012-11-15 11:57:02 -0400

    Creating data modeler classes

---
+++ src/com/budgetmanage/entities/Budget.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.budgetmanage.entities;
+
+import com.budgetmanage.util.Entities;
+
+/**
+ *
+ * @author Nestor_Velasquez
+ */
+public class Budget extends Finance implements Entities{
+
+    public Budget() {
+    }
+    
+    public Budget(double amount, int concept) {
+        setAmount(amount);
+        setConcept(concept);
+    }
+    
+    
+    @Override
+    public Finance get() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Finance get(String name) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void add() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void modify() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void delete() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setConcept(int concept) {
+        this.concept = concept;
+    }
+
+    public int getConcept() {
+        return concept;
+    }
+}
---
+++ src/com/budgetmanage/entities/Expending.java
@@ -0,0 +1,57 @@
+package com.budgetmanage.entities;
+
+import com.budgetmanage.util.Entities;
+
+/**
+ *
+ * @author Nestor_Velasquez
+ */
+public class Expending extends Finance implements Entities{   
+
+    public Expending(double amount, int concept) {
+        setAmount(amount);
+        setConcept(concept);
+    }
+    
+    @Override
+    public Finance get() {
+        return this;
+    }
+
+    @Override
+    public Finance get(String name) {
+        return this;
+    }
+    
+    public double getAmount() {
+        return getAmount();
+    }
+
+    public void setAmount(double amount) {
+        this.setAmount(amount);
+    }
+
+    public void setConcept(int concept) {
+        this.setConcept(concept);
+    }
+
+    public int getConcept() {
+        return getConcept();
+    }    
+
+    @Override
+    public void add() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void modify() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void delete() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
