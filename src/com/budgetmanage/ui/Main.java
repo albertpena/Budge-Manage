fd465754a59bc51b75c09fb4701bdb0e7f382c10
Nestor_Velasquez <Nestor_Velasquez@opmobile02>
2012-11-20 00:12:02 -0400

    Modifying and adding forms

--- src/com/budgetmanage/ui/Main.java
+++ src/com/budgetmanage/ui/Main.java
@@ -6,21 +6,24 @@ package com.budgetmanage.ui;
 
 import com.budgetmanage.ui.Consulting.FinancesConsultingFrm;
 import com.budgetmanage.ui.Maintenance.FinancesMaintenanceFrm;
+import com.budgetmanage.ui.maintenance.FinancesAddFrm;
+import com.budgetmanage.util.Constant;
 import java.awt.BorderLayout;
 
 /**
  *
  * @author Fulvio
  */
-public class Main extends javax.swing.JFrame {
+public class Main extends javax.swing.JFrame implements Constant{
 
     /**
      * Creates new form Main
      */
     public Main() {
         initComponents();
-        setSize(600, 600);
+        setSize(FRAME_WIDTH, FRAME_HEIGHT);
         this.setLayout(new BorderLayout());
+        dim = this.getPreferredSize();
     }
 
     /**
@@ -36,52 +39,54 @@ public class Main extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu4 = new javax.swing.JMenu();
-        jMenu1 = new javax.swing.JMenu();
-        jMenuItem19 = new javax.swing.JMenuItem();
-        jMenuItem20 = new javax.swing.JMenuItem();
-        jMenuItem33 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        jMenuItem26 = new javax.swing.JMenuItem();
+        jMenuItem27 = new javax.swing.JMenuItem();
         jMenu3 = new javax.swing.JMenu();
         jMenuItem25 = new javax.swing.JMenuItem();
         jMenuItem23 = new javax.swing.JMenuItem();
         jMenuItem24 = new javax.swing.JMenuItem();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem19 = new javax.swing.JMenuItem();
+        jMenuItem20 = new javax.swing.JMenuItem();
+        jMenuItem33 = new javax.swing.JMenuItem();
 
         jMenuItem1.setText("jMenuItem1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
         getContentPane().setLayout(null);
 
         jPanel1.setBackground(new java.awt.Color(204, 255, 204));
         jPanel1.setLayout(new java.awt.BorderLayout());
         getContentPane().add(jPanel1);
         jPanel1.setBounds(0, 0, 770, 550);
+        jPanel1.setPreferredSize(this.getPreferredSize());
 
-        jMenu4.setText("Start");
+        jMenu4.setText("Inicio");
         jMenuBar1.add(jMenu4);
 
-        jMenu1.setText("Maintenance");
+        jMenu2.setText("Agregar");
 
-        jMenuItem19.setText("Account");
-        jMenu1.add(jMenuItem19);
+        jMenuItem26.setText("Cuentas");
+        jMenu2.add(jMenuItem26);
 
-        jMenuItem20.setText("Finanza");
-        jMenuItem20.addActionListener(new java.awt.event.ActionListener() {
+        jMenuItem27.setText("Finanzas");
+        jMenuItem27.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem20ActionPerformed(evt);
+                jMenuItem27ActionPerformed(evt);
             }
         });
-        jMenu1.add(jMenuItem20);
+        jMenu2.add(jMenuItem27);
 
-        jMenuItem33.setText("User");
-        jMenu1.add(jMenuItem33);
+        jMenuBar1.add(jMenu2);
 
-        jMenuBar1.add(jMenu1);
+        jMenu3.setText("Consultas");
 
-        jMenu3.setText("Consulting");
-
-        jMenuItem25.setText("Account");
+        jMenuItem25.setText("Cuentas");
         jMenu3.add(jMenuItem25);
 
-        jMenuItem23.setText("Finances");
+        jMenuItem23.setText("Finanzas");
         jMenuItem23.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem23ActionPerformed(evt);
@@ -89,10 +94,33 @@ public class Main extends javax.swing.JFrame {
         });
         jMenu3.add(jMenuItem23);
 
-        jMenuItem24.setText("Loan");
+        jMenuItem24.setText("Prestamos");
+        jMenuItem24.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem24ActionPerformed(evt);
+            }
+        });
         jMenu3.add(jMenuItem24);
 
         jMenuBar1.add(jMenu3);
+
+        jMenu1.setText("Mantenimientos");
+
+        jMenuItem19.setText("Cuentas");
+        jMenu1.add(jMenuItem19);
+
+        jMenuItem20.setText("Finanzas");
+        jMenuItem20.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem20ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem20);
+
+        jMenuItem33.setText("Usuario");
+        jMenu1.add(jMenuItem33);
+
+        jMenuBar1.add(jMenu1);
 
         setJMenuBar(jMenuBar1);
     }// </editor-fold>//GEN-END:initComponents
@@ -104,8 +132,21 @@ public class Main extends javax.swing.JFrame {
 
     private void jMenuItem20ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem20ActionPerformed
         jPanel1.removeAll();
-        jPanel1.add(new FinancesMaintenanceFrm(), BorderLayout.CENTER);
+        FinancesMaintenanceFrm fmf = new FinancesMaintenanceFrm();
+        fmf.setPreferredSize(dim);
+        jPanel1.add(fmf, BorderLayout.CENTER);
     }//GEN-LAST:event_jMenuItem20ActionPerformed
+
+    private void jMenuItem27ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem27ActionPerformed
+        jPanel1.removeAll();
+        FinancesAddFrm fad = new FinancesAddFrm();
+        fad.setPreferredSize(dim);
+        jPanel1.add(fad, BorderLayout.CENTER);
+    }//GEN-LAST:event_jMenuItem27ActionPerformed
+
+    private void jMenuItem24ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem24ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem24ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -141,8 +182,10 @@ public class Main extends javax.swing.JFrame {
             }
         });
     }
+    private java.awt.Dimension dim;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuBar jMenuBar1;
@@ -152,6 +195,8 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem23;
     private javax.swing.JMenuItem jMenuItem24;
     private javax.swing.JMenuItem jMenuItem25;
+    private javax.swing.JMenuItem jMenuItem26;
+    private javax.swing.JMenuItem jMenuItem27;
     private javax.swing.JMenuItem jMenuItem33;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
