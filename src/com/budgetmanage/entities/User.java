b9d250d95f787d4d14a85f2200f9592238f7b79e
Fulvio Moya <fulvio.127@gmail.com>
2012-11-27 14:42:47 -0400

    Adding the project User and UserJpaController

--- src/com/budgetmanage/entities/User.java
+++ src/com/budgetmanage/entities/User.java
@@ -5,7 +5,6 @@
 package com.budgetmanage.entities;
 
 import java.io.Serializable;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,17 +16,9 @@ import javax.persistence.Id;
  */
 @Entity
 public class User implements Serializable {
-    @Column(nullable=false, length=50)
-    private String password;
-    @Column(unique=true)
-    private String userName;
-    @Column(nullable=false)
-    private boolean status;
-    
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    
     private Long id;
 
     public Long getId() {
@@ -61,48 +52,6 @@ public class User implements Serializable {
     @Override
     public String toString() {
         return "com.budgetmanage.entities.User[ id=" + id + " ]";
-    }
-
-    /**
-     * @return the password
-     */
-    public String getPassword() {
-        return password;
-    }
-
-    /**
-     * @param password the password to set
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    /**
-     * @return the userName
-     */
-    public String getUserName() {
-        return userName;
-    }
-
-    /**
-     * @param userName the userName to set
-     */
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    /**
-     * @return the status
-     */
-    public boolean isStatus() {
-        return status;
-    }
-
-    /**
-     * @param status the status to set
-     */
-    public void setStatus(boolean status) {
-        this.status = status;
     }
     
 }
