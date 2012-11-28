587202ec89257625c2b90f6b8f54ba8f099cd1d2
fulviomya <fulvio.127@gmail.com>
2012-11-28 14:12:41 -0400

    Adding new properties to BudgetUser Class

--- src/com/budgetmanage/entities/BudgetUser.java
+++ src/com/budgetmanage/entities/BudgetUser.java
@@ -19,6 +19,12 @@ public class BudgetUser implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    private String  nombre;
+    private String  apellidos;
+    private String  userName;
+    private String  password;
+    private boolean status_User;
+    
     private Long id;
 
     public Long getId() {
@@ -52,6 +58,76 @@ public class BudgetUser implements Serializable {
     @Override
     public String toString() {
         return "com.budgetmanage.entities.User[ id=" + id + " ]";
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellidos
+     */
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    /**
+     * @param apellidos the apellidos to set
+     */
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the status_User
+     */
+    public boolean isStatus_User() {
+        return status_User;
+    }
+
+    /**
+     * @param status_User the status_User to set
+     */
+    public void setStatus_User(boolean status_User) {
+        this.status_User = status_User;
     }
     
 }
