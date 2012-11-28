3826dd7d78e303112b0a67466e240c5effe2a6a2
Nestor_Velasquez <Nestor_Velasquez@opmobile02>
2012-11-28 18:21:48 -0400

    New class PieChar add for make a chart

---
+++ src/com/budgetmanage/util/PieChart.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.budgetmanage.util;
+
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
+
+/**
+ *
+ * @author Nestor_Velasquez
+ */
+public class PieChart extends JComponent {
+    Font font;
+
+    @Override
+    public void paint(Graphics grphcs) {
+        Graphics2D g2 = (Graphics2D) grphcs;
+    }    
+    
+}
