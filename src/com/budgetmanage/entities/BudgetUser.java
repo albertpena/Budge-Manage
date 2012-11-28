cb44fab8e8d122190b2974201763c5980a613a39
fulviomya <fulvio.127@gmail.com>
2012-11-28 03:06:48 -0400

    Rename a EntityClass

--- src/com/budgetmanage/entities/BudgetUser.java
+++ src/com/budgetmanage/entities/BudgetUser.java
@@ -15,7 +15,7 @@ import javax.persistence.Id;
  * @author Fulvio
  */
 @Entity
-public class User implements Serializable {
+public class BudgetUser implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -39,10 +39,10 @@ public class User implements Serializable {
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof User)) {
+        if (!(object instanceof BudgetUser)) {
             return false;
         }
-        User other = (User) object;
+        BudgetUser other = (BudgetUser) object;
         if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
             return false;
         }
